3bd2433f6278b2085048a2725a8b8ad956bfd732
YahavGanon <yahavganon86@gmail.com>
2024-02-13 17:24:08 +0200

    Make new util.js file and pit function getRandom and connect it in the dom

---
+++ js/util.js
@@ -0,0 +1,6 @@
+'use strict'
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
+}
