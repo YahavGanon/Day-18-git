e5b7cb59ffd4a9ce948b186bb93ee091083d0b98
YahavGanon <yahavganon86@gmail.com>
2024-02-13 17:39:13 +0200

    Change onBallClick to give a random size each click

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
 const onBallClick = () => {
     const elBall = document.querySelector('.ball')
     const currWidth = elBall.clientWidth
-    elBall.style.width = currWidth + 50 + 'px'
+    elBall.style.width = currWidth + getRandomIntInclusive(20, 60) + 'px'
     const currHeight = elBall.clientHeight
-    elBall.style.height = currHeight + 50 + 'px'
+    elBall.style.height = currHeight + getRandomIntInclusive(20, 60) + 'px'
     elBall.innerHTML = currWidth + currHeight
     if (currWidth + currHeight > 400) {
         elBall.style.width = 100 + 'px'
