7469b408f898d691a26155ef027012a188d53b97
YahavGanon <yahavganon86@gmail.com>
2024-02-13 17:51:52 +0200

    Add the getrandomcolor function to the util.js file and change the onBallClick function that on each click it will change the color

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@ const onBallClick = () => {
     elBall.style.width = currWidth + getRandomIntInclusive(20, 60) + 'px'
     const currHeight = elBall.clientHeight
     elBall.style.height = currHeight + getRandomIntInclusive(20, 60) + 'px'
+    elBall.style.backgroundColor = getRandomColor() 
     elBall.innerHTML = currWidth + currHeight
     if (currWidth + currHeight > 400) {
         elBall.style.width = 100 + 'px'
         elBall.style.height = 100 + 'px'
         elBall.innerText = 100
+        elBall.style.backgroundColor = 'aquamarine'
     }
 }
 
