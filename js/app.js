96479f0a8c6040db0965e62b6a5de4690d95215e
YahavGanon <yahavganon86@gmail.com>
2024-02-13 18:43:15 +0200

    Made another ball gave him different elemnts and diffrent maxDiameter

--- js/app.js
+++ js/app.js
@@ -15,5 +15,22 @@ const onBallClick = () => {
         elBall.style.backgroundColor = 'aquamarine'
     }
 }
+ 
+const onBallClick2 = () => {
+    const elBall = document.querySelector('.ball2')
+    const currWidth = elBall.clientWidth
+    elBall.style.width = currWidth + getRandomIntInclusive(20, 60) + 'px'
+    const currHeight = elBall.clientHeight
+    elBall.style.height = currHeight + getRandomIntInclusive(20, 60) + 'px'
+    elBall.style.backgroundColor = getRandomColor() 
+    elBall.innerHTML = currWidth + currHeight
+    if (currWidth + currHeight > 600) {
+        elBall.style.width = 100 + 'px'
+        elBall.style.height = 100 + 'px'
+        elBall.innerText = 100
+        elBall.style.backgroundColor = 'yellow'
+    }
+
+}
 
 
