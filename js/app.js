b76735af89e9b9b7b9751a06f7e1e19a0955d99a
YahavGanon <yahavganon86@gmail.com>
2024-02-13 15:57:53 +0200

    Make changes that in each click of user width and height go up by 50 px each

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 'use strict'
 
 const onBallClick = () => {
-    console.log('Hello world')
+    const elBall = document.querySelector('.ball')
+    const currWidth = elBall.clientWidth
+    elBall.style.width = currWidth + 50 + 'px'
+    const currHeight = elBall.clientHeight
+    elBall.style.height = currHeight + 50 + 'px'
+    elBall.innerHTML = currWidth + currHeight
 }
+
+
