787ccd59b882503ac95555c308b20580cfcb9834
YahavGanon <yahavganon86@gmail.com>
2024-02-13 20:29:49 +0200

    Made changes in ball num4 at html and java script

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 'use strict'
+var maxDiameter = 400
+var maxDiameter2 = 600
 
 const onBallClick = () => {
-    const maxDiameter = 400
+    // const maxDiameter = 400
     const elBall = document.querySelector('.ball')
     const currWidth = elBall.clientWidth
     elBall.style.width = currWidth + getRandomIntInclusive(20, 60) + 'px'
@@ -18,7 +20,7 @@ const onBallClick = () => {
 }
 
 const onBallClick2 = () => {
-    const maxDiameter = 600
+    // const maxDiameter = 600
     const elBall = document.querySelector('.ball2')
     const currWidth = elBall.clientWidth
     elBall.style.width = currWidth + getRandomIntInclusive(20, 60) + 'px'
@@ -26,12 +28,22 @@ const onBallClick2 = () => {
     elBall.style.height = currHeight + getRandomIntInclusive(20, 60) + 'px'
     elBall.style.backgroundColor = getRandomColor()
     elBall.innerHTML = currWidth + currHeight
-    if (currWidth + currHeight > maxDiameter) {
+    if (currWidth + currHeight > maxDiameter2) {
         elBall.style.width = 100 + 'px'
         elBall.style.height = 100 + 'px'
         elBall.innerText = 100
         elBall.style.backgroundColor = 'yellow'
     }
+}
+
+const onBallFour = () => {
+    const elBall = document.querySelector('.ball')
+    const elBall2 = document.querySelector('.ball2')
+    maxDiameter = maxDiameter - getRandomIntInclusive(20, 60)
+    maxDiameter2 = maxDiameter2 - getRandomIntInclusive(20, 60)
+
+if(maxDiameter === 100) return maxDiameter = 400
+if(maxDiameter2 === 100) return maxDiameter2 = 600
 }
 
 const onFifthBall = () => {
@@ -49,12 +61,14 @@ const onSixBall = () => {
     elBall.style.height = 100 + 'px'
     elBall.innerText = 100
     elBall.style.backgroundColor = 'green'
-    
-    
+    maxDiameter = 400
+
+
     elBall2.style.width = 100 + 'px'
     elBall2.style.height = 100 + 'px'
     elBall2.innerText = 100
     elBall2.style.backgroundColor = 'yellow'
+    maxDiameter2 = 600
 }
 
 
