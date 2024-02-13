54fcf20778806e1b1ea62d5745d3c0648ab54ca2
YahavGanon <yahavganon86@gmail.com>
2024-02-13 16:47:08 +0200

    Update the function and added limits to ball size

--- js/app.js
+++ js/app.js
@@ -7,6 +7,11 @@ const onBallClick = () => {
     const currHeight = elBall.clientHeight
     elBall.style.height = currHeight + 50 + 'px'
     elBall.innerHTML = currWidth + currHeight
+    if (currWidth + currHeight > 400) {
+        elBall.style.width = 100 + 'px'
+        elBall.style.height = 100 + 'px'
+        elBall.innerText = 100
+    }
 }
 
 
